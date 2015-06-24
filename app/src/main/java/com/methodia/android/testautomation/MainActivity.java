f52222df83b2f5f8cadfc8353da0a1fffe11306a
Stefan Doychev <sdoychev@gmail.com>
2015-06-24 17:00:04 +0300

    Merge.

--- app/src/main/java/com/methodia/android/testautomation/MainActivity.java
+++ app/src/main/java/com/methodia/android/testautomation/MainActivity.java
@@ -2,6 +2,7 @@ package com.methodia.android.testautomation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
